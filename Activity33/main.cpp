4684aa4e9a5721e2dbd88eca106a989aed0cfa3d
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-12-01 20:50:15 -0800

    Initial Commit

---
+++ Activity33/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Activity33
+//
+//  Created by Chatchapong Thuaypha on 12/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
