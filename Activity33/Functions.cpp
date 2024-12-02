60aa1bb226a046280828b6e7cb18e388d5c23bf6
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-12-01 20:51:19 -0800

    first commit

---
+++ Activity33/Functions.cpp
@@ -0,0 +1,8 @@
+//
+//  Functions.cpp
+//  Activity33
+//
+//  Created by Chatchapong Thuaypha on 12/1/24.
+//
+
+#include "Functions.hpp"
---
+++ Activity33/Functions.hpp
@@ -0,0 +1,13 @@
+//
+//  Functions.hpp
+//  Activity33
+//
+//  Created by Chatchapong Thuaypha on 12/1/24.
+//
+
+#ifndef Functions_hpp
+#define Functions_hpp
+
+#include <stdio.h>
+
+#endif /* Functions_hpp */
